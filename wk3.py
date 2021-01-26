9900ff2ed51e83db5dff2be1eff228058c7ffc88
Ellys22 <sba21167@student.cct.ie>
2021-01-26 21:20:49 +0000

    save wk3 changes

---
+++ wk3.py
@@ -0,0 +1,56 @@
+#print(5 + 3) # Prints: 8
+#sum_value = 5 + 3
+#print(sum_value) # Prints 8
+
+#my_list = [] # Initialize an empty list
+
+#name = "John" # Initialize name variable to John
+
+#my_list = my_list + name # Adding the current my_list variable to the string name
+
+#print(my_list) # Prints: ['j','o','h','n']
+
+#my_list = [1,2,3,4] # Initialize my_list
+
+#my_list = my_list * 3 # Take my_list and multiply it by 3
+
+#print(my_list) # Prints: [1,2,3,4,1,2,3,4,1,2,3,4]
+
+#print(5 > 3) # Prints: True; since 5 is greater than 3
+
+#result = 5 > 3 # You can store the result in a variable
+
+#print(result) # Prints: True
+
+#print(3 > 5) # Prints: False; since 3 is NOT greater than 5
+
+#print(5 > 5) # Prints: False; since 5 is NOT greater than 5 (they are equal)
+
+#x = 2 # Setting up the x variable
+
+#if x < 3:
+   # x += 2 # This will run if x < 3, otherwise it will be skipped over
+
+#print(x) # Since this is on a lower indentation level, this code will run regardless
+
+#if x < 3:
+  #  x += 2 # This will run if x < 3, otherwise it will be skipped over
+
+#print(x) # Since this is on a lower indentation level, this code will run regardless
+
+
+x = 2 # Setting up the x variable
+
+if x < 3:
+    x += 2 # This will run if x < 3, otherwise it will be skipped over
+else:
+    x -= 1 # This will run if x is not less than 3
+
+print(x) # Since this is on a lower indentation level, this code will run regardless
+
+if x < 3:
+    x += 2 # This will run if x < 3, otherwise it will be skipped over
+else:
+    x -= 1 # This will run if x is not less than 3
+
+print(x) # Since this is on a lower indentation level, this code will run regardless
