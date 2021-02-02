4ea5797389001c81d7264052e8e4d31f37149734
Ellys22 <sba21167@student.cct.ie>
2021-02-02 21:25:01 +0000

    save

---
+++ wk4.py
@@ -0,0 +1,21 @@
+# x = 0 # Setup a variable to use for the conditional
+
+# while x <= 10: # Continue looping until x is greater than 10
+#     print(x) # Print the current itterations value of x
+#     x += 1 # Inrement x by 1 (add 1 to the current value of x)
+
+# print("loop Finished") # This will execute after the loop since it's at a lower indentation level
+
+# shopping_list = ["Eggs", "Ham", "Milk"]
+
+# for item in shopping_list: # Iterate through the shopping list
+#     print(item) # Print the item at the current iteration
+
+# greeting = "Hello-World" # Setting up a string to iterate through
+
+# for character in greeting: # Iterate over the string one letter at a time
+#     if "-" in character: # if the current character is a hyphen
+#         print("Hyphen detected, ending loop!")
+#         break # End the loop
+#     else:
+#         print(character) # Print the current character
