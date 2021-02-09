11bece35a98487af58c62bb5452fff07c02a5e84
Ellys22 <sba21167@student.cct.ie>
2021-02-09 20:55:11 +0000

    save

--- wk5.py
+++ wk5.py
@@ -45,24 +45,42 @@
 
 # leopard_gecko.print_info()
 
-class CookieBaker:
-  def __init__(self, number_of_cookies):
-    """ Example class that is used to show off the __init__ method.
-    The __init__ method calls the bake_cookie() method as many times as there are number_of_cookies.
+# class CookieBaker:
+#   def __init__(self, number_of_cookies):
+#     """ Example class that is used to show off the __init__ method.
+#     The __init__ method calls the bake_cookie() method as many times as there are number_of_cookies.
 
-    Attributes
-    ----------
-    number_of_cookies(int): How many cookies to bake
-    """
-    print(f"__init__ method called, creating {number_of_cookies} cookie(s):")
-    self.number_of_cookies = number_of_cookies
-    for cookie in range(number_of_cookies):
-      self.bake_cookie()
+#     Attributes
+#     ----------
+#     number_of_cookies(int): How many cookies to bake
+#     """
+#     print(f"__init__ method called, creating {number_of_cookies} cookie(s):")
+#     self.number_of_cookies = number_of_cookies
+#     for cookie in range(number_of_cookies):
+#       self.bake_cookie()
 
-  def bake_cookie(self):
-    """Print's 'Cookie Baked!'."""
-    print("Cookie Baked!")
+#   def bake_cookie(self):
+#     """Print's 'Cookie Baked!'."""
+#     print("Cookie Baked!")
 
-    cookies = CookieBaker(10)
-    cookies.bake_cookie()
+#     cookies = CookieBaker(10)
+#     cookies.bake_cookie()
 
+# import datetime
+# class user:
+#   def __init__(self, name, age, sign_up_date, birthday, premium_member):
+#     """A class to represent a generic animal
+
+#     Attributes
+#     ----------
+#     name(str): The technical species name of the animal
+#     age(str): A list of regions the animal is endemic to
+#     sign_up_date(datetime.datetime): A datetime object of the day the user signed up
+#     birthday(datetime.datetime): A datetime object of the users birthday
+#     premium_member(bool): Whether the user is on premium or free subscription
+#     """
+#     self.name = name
+#     self.age = age
+#     self.sign_up_date = sign_up_date
+#     self.birthday = birthday
+#     self.premium_member = premium_member
